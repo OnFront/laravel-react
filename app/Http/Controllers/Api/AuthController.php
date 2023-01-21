21e82a6717bace67ec899f4996dfe6b64ec76c97
LAPTOP-6I0706AC\jaros <jaroslaw.decowski@projectpeople.pl>
2023-01-21 21:18:00 +0100

    add routes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,18 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+
+    }
+
+    public function signup()
+    {
+
+    }
+
+    public function logout()
+    {
+        
+    }
 }
