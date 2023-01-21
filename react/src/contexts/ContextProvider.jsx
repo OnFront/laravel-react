3aee04187849495445e96418f683bcf93bac1045
LAPTOP-6I0706AC\jaros <jaroslaw.decowski@projectpeople.pl>
2023-01-21 19:53:57 +0100

    fix context provider

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useContext} from "react";
 
 const StateContext = createContext({
-  currentUser: null,
+  user: null,
   token: null,
   setUser: () => {},
   setToken: () => {}
@@ -16,20 +16,22 @@ export const ContextProvider = ({children}) => {
     _setToken(token)
 
     if(token) {
-      localStorage.setItem = ('ACCESS_TOKEN', token);
+      localStorage.setItem('ACCESS_TOKEN', token);
     } else {
       localStorage.removeItem('ACCESS_TOKEN');
     }
   }
 
   return (
+
     <StateContext.Provider value={{
       user,
       token,
       setUser,
       setToken
-    }}/>
-
+    }}>
+      {children}
+    </StateContext.Provider>
   )
 }
 
